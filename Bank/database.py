852b6f35e41b9e1fcccb2c11f4c6430f3154814c
Worathep Seenuan <s.worathep@Woratheps-Mac-mini.local>
2026-01-08 02:22:29 +0700

    Fix SQLite path to absolute path

--- Bank/database.py
+++ Bank/database.py
@@ -1,13 +1,17 @@
-# Database Conection เชื่อมต่อกับ SQLite
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
+import os
 
-# สร้าง SQLite Database
-DATABASE_URL = "sqlite:///bank.db"
+# --- จุดที่แก้ไข: หาที่อยู่ไฟล์จริง (Absolute Path) ---
+# หาว่าไฟล์ database.py นี้อยู่ที่โฟลเดอร์ไหนในเครื่อง Server
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Engine ตัวจัดกการฐานข้อมูล
+# สั่งให้สร้าง bank.db ไว้ในโฟลเดอร์เดียวกันเลย (จะไม่มีทางหลงที่แน่นอน)
+DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'bank.db')}"
+# --------------------------------------------------
+
+# Engine ตัวจัดการฐานข้อมูล
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}, echo=False)
 
 # Session ใช้ทำ transaction 
--- smart_client.py
+++ smart_client.py
@@ -1,71 +1,91 @@
-from locust import HTTPUser, task, between
+from locust import HttpUser, task, between
 import requests
 import uuid
 import json
 
 
-BANK_API_URL = "http://127.0.0.1:8000"
+BANK_API_URL = "http://188.166.254.136"
 
-class SmartClient(HTTPUser):
+class SmartClient(HttpUser):
     wait_time = between(1, 3)
 
     def on_start(self):
 
         # เริ่มต้นผู้ใช้แต่ละคน
         self.user_id = None
-        self.token = None
+        self.tokens = []
         self.prepare_bank_account()
 
     def prepare_bank_account(self):
         try:
             # สมัครสมาชิก
             username = f"user_{str(uuid.uuid4())[0:8]}"
-            response_register = requests.post(f"{BANK_API_URL}/users/", json={
+            response_register = self.client.post(
+                f"{BANK_API_URL}/users/", json={
                 "username": username,
                 "password": "password123",
                 "initial_balance": 0.0
-            })
+                },
+                name ="/users/ [register]"
+            )
+
             if response_register.status_code == 200:
                 self.user_id = response_register.json()['id']
 
-                requests.post(f"{BANK_API_URL}/topup/", json={
+                topup_response = self.client.post(
+                    f"{BANK_API_URL}/topup/", json={
                     "user_id": self.user_id,
-                    "amount": 100.0
-                })
+                    "amount": 100.0},
+                    name = "/topup/"
+                )
+                if topup_response.status_code != 200:
+                    print(f"Topup failed: {topup_response.text}")
             else:
                 print(f"Register failed: {response_register.text}")
+
         except Exception as e:
             print(f"Bank Connection Error: {e}")
     @task
     def access_service_with_payment(self):
         
+        if not self.user_id:
+            return
+        
         headers = {}
-        if self.token:
-            headers = {"X-Payment-Token": self.token}
+        if self.tokens:
+            headers = {"X-Payment-Token": self.tokens}
         
         with self.client.get("/premium-data", headers=headers, catch_response=True) as response:
 
             if response.status_code == 402:
-                response.failure("402 Payment Required")
+                response.success()
 
                 self.buy_token()
 
             elif response.status_code == 200:
                 response.success()
             
+                if self.tokens:
+                    self.tokens.pop(0)
+
             else:
                 response.failure(f"Unexpected Status: {response.status_code}")
     
     def buy_token(self):
+        if not self.user_id:
+            return
+        
         try:
-            response_buy = requests.post(f"{BANK_API_URL}/purchase/", json={
+            response_buy = self.client.post(
+                f"{BANK_API_URL}/purchase/", json={
                 "user_id": self.user_id,
-                "quantity": 50
-            })
+                "quantity": 50},
+                name ="/purchase/"    
+            )
 
             if response_buy.status_code == 200:
                 token_data = response_buy.json()
-                if 'token' in token_data and len(token_data['tokens']) > 0:
-                    self.token = token_data['tokens'][0]
+                if 'tokens' in token_data:
+                    self.tokens.extend(token_data['tokens'])
         except Exception as e:
             print(f"Purchase Error: {e}")
